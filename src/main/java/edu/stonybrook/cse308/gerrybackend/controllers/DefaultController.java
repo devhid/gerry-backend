aaae28a4fe2ed0c319fc2ebaf5ccac9f77523169
devhid <devhid@users.noreply.github.com>
2019-09-12 18:48:34 -0400

    Added default controller and integrated Swagger

---
+++ src/main/java/edu/stonybrook/cse308/gerrybackend/controllers/DefaultController.java
@@ -0,0 +1,15 @@
+package edu.stonybrook.cse308.gerrybackend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @RequestMapping(path="/", method=RequestMethod.GET)
+    public String index() {
+        return "200\n";
+    }
+}
