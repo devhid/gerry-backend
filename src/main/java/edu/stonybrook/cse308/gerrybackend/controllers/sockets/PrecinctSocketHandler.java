f99d4b7bc5249d97105b69e2a2acba5193f8f8c7
spiderpig86 <slim679975@gmail.com>
2019-11-18 00:21:54 -0500

    [WS] Added logic for buffering websocket requests for fetching precincts

--- src/main/java/edu/stonybrook/cse308/gerrybackend/controllers/sockets/PrecinctSocketHandler.java
+++ src/main/java/edu/stonybrook/cse308/gerrybackend/controllers/sockets/PrecinctSocketHandler.java
@@ -1,10 +1,12 @@
 package edu.stonybrook.cse308.gerrybackend.controllers.sockets;
 
 import edu.stonybrook.cse308.gerrybackend.db.repositories.PrecinctRepository;
+import edu.stonybrook.cse308.gerrybackend.db.repositories.StateRepository;
 import edu.stonybrook.cse308.gerrybackend.db.services.PrecinctService;
 import edu.stonybrook.cse308.gerrybackend.enums.types.StateType;
 import edu.stonybrook.cse308.gerrybackend.graph.nodes.PrecinctNode;
 import edu.stonybrook.cse308.gerrybackend.graph.nodes.StateNode;
+import edu.stonybrook.cse308.gerrybackend.utils.PrecinctSocketBuffer;
 import edu.stonybrook.cse308.gerrybackend.utils.UriUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,29 +18,52 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Component
 public class PrecinctSocketHandler extends TextWebSocketHandler {
 
+    private final int BATCH_SIZE = 100;
+
+    @Autowired
+    private StateRepository stateRepository;
     private static final Logger LOGGER = LoggerFactory.getLogger(PrecinctSocketHandler.class);
 
     @Override
     public void afterConnectionEstablished(final WebSocketSession session) throws Exception {
         LOGGER.info(String.format("Connected to session %s.", session.getId()));
-        // TODO: Immediately send precinct data upon connection?
-        // TODO: Extract the 'geography' field from the object since that contains well-formed JSON for FE
         final String state = UriUtils.getLastPath(session.getUri());
         Set<PrecinctNode> precincts = null;
         if (state.equals(StateType.CALIFORNIA.getName())) {
             LOGGER.info("CA");
+            StateNode stateNode = stateRepository.findById(StateType.CALIFORNIA.getName()).get();
+            precincts = stateNode.getAllPrecincts();
         } else if (state.equals(StateType.UTAH.getName())) {
             LOGGER.info("UT");
+            StateNode stateNode = stateRepository.findById(StateType.UTAH.getName()).get();
+            precincts = stateNode.getAllPrecincts();
         } else if (state.equals(StateType.VIRGINIA.getName())) {
             LOGGER.info("VA");
+            StateNode stateNode = stateRepository.findById(StateType.VIRGINIA.getName()).get();
+            precincts = stateNode.getAllPrecincts();
         } else {
-            LOGGER.error("Invalid state detected.");
+            LOGGER.error("Invalid state selected.");
         }
-        session.close(new CloseStatus(CloseStatus.NORMAL.getCode(), "Completed fetching precincts"));
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Future<?> future = executorService.submit(new PrecinctSocketBuffer(session, precincts, BATCH_SIZE));
+
+        CloseStatus closeStatus = new CloseStatus(CloseStatus.NORMAL.getCode(), "Completed fetching precincts.");
+        try {
+            future.get();
+        } catch (final ExecutionException e) {
+            closeStatus = new CloseStatus(CloseStatus.SERVER_ERROR.getCode(), "Error transmitting precincts.");
+        } finally {
+            session.close(closeStatus);
+        }
     }
 
     @Override
