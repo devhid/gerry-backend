a9e1ccba0f8a5214b42c964293e98ba201bb881a
jso123450 <jso123450@gmail.com>
2019-11-17 23:02:15 -0500

    Changed SortedSet back to Set. Should not be needed.

--- src/main/java/edu/stonybrook/cse308/gerrybackend/data/algorithm/CandidatePairs.java
+++ src/main/java/edu/stonybrook/cse308/gerrybackend/data/algorithm/CandidatePairs.java
@@ -1,22 +1,14 @@
 package edu.stonybrook.cse308.gerrybackend.data.algorithm;
 
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 public class CandidatePairs {
 
-    private SortedSet<LikelyCandidatePair> majorityMinorityPairs;
-    private SortedSet<LikelyCandidatePair> otherPairs;
+    private Set<LikelyCandidatePair> majorityMinorityPairs;
+    private Set<LikelyCandidatePair> otherPairs;
 
     public CandidatePairs(Set<LikelyCandidatePair> majorityMinorityPairs,
                           Set<LikelyCandidatePair> otherPairs){
-        this.majorityMinorityPairs = new TreeSet<>(majorityMinorityPairs);
-        this.otherPairs = new TreeSet<>(otherPairs);
-    }
-
-    public CandidatePairs(SortedSet<LikelyCandidatePair> majorityMinorityPairs,
-                          SortedSet<LikelyCandidatePair> otherPairs){
         this.majorityMinorityPairs = majorityMinorityPairs;
         this.otherPairs = otherPairs;
     }
