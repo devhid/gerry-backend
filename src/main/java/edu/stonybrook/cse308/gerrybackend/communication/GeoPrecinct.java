8b4292ce35c61921683c00bfde9faac8aa9ec93d
jso123450 <jso123450@gmail.com>
2019-11-24 23:15:04 -0500

    Added wrapping communication class for sending precinct data.

---
+++ src/main/java/edu/stonybrook/cse308/gerrybackend/communication/GeoPrecinct.java
@@ -0,0 +1,22 @@
+package edu.stonybrook.cse308.gerrybackend.communication;
+
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import edu.stonybrook.cse308.gerrybackend.graph.nodes.PrecinctNode;
+
+public class GeoPrecinct {
+
+    private String id;
+
+    @JsonRawValue
+    private String geometry;
+
+    public GeoPrecinct(String id, String geometry){
+        this.id = id;
+        this.geometry = geometry;
+    }
+
+    public static GeoPrecinct fromPrecinctNode(PrecinctNode precinct){
+        return new GeoPrecinct(precinct.getId(), precinct.getGeometryJson());
+    }
+
+}
--- src/main/java/edu/stonybrook/cse308/gerrybackend/controllers/sockets/PrecinctSocketHandler.java
+++ src/main/java/edu/stonybrook/cse308/gerrybackend/controllers/sockets/PrecinctSocketHandler.java
@@ -1,5 +1,8 @@
 package edu.stonybrook.cse308.gerrybackend.controllers.sockets;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.stonybrook.cse308.gerrybackend.communication.GeoPrecinct;
 import edu.stonybrook.cse308.gerrybackend.db.services.StateService;
 import edu.stonybrook.cse308.gerrybackend.enums.types.StateType;
 import edu.stonybrook.cse308.gerrybackend.graph.nodes.PrecinctNode;
@@ -15,11 +18,13 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 @Component
 public class PrecinctSocketHandler extends TextWebSocketHandler {
@@ -28,6 +33,10 @@ public class PrecinctSocketHandler extends TextWebSocketHandler {
 
     @Autowired
     private StateService stateService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(PrecinctSocketHandler.class);
 
     @Override
@@ -44,8 +53,20 @@ public class PrecinctSocketHandler extends TextWebSocketHandler {
         }
         closeStatus = new CloseStatus(CloseStatus.NORMAL.getCode(), "Completed fetching precincts.");
         final Set<PrecinctNode> precincts = stateNode.getAllPrecincts();
+        final List<GeoPrecinct> transformedPrecincts = precincts.stream()
+                .map(GeoPrecinct::fromPrecinctNode)
+                .collect(Collectors.toList());
+        final List<String> precinctJson = transformedPrecincts.stream()
+                .map(p -> {
+                    try {
+                        return objectMapper.writeValueAsString(p);
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }).collect(Collectors.toList());
         final ExecutorService executorService = Executors.newFixedThreadPool(1);
-        Future<?> future = executorService.submit(new PrecinctSocketBuffer(session, precincts, BATCH_SIZE));
+        Future<?> future = executorService.submit(new PrecinctSocketBuffer(session, precinctJson, BATCH_SIZE));
 
         try {
             future.get();
