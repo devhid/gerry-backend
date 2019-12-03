d20ebd0fe818d6884feb9709a38aa217c8ae7755
jso123450 <jso123450@gmail.com>
2019-12-03 17:00:39 -0500

    Changed ElectionType enum names to lowercase.

--- src/main/java/edu/stonybrook/cse308/gerrybackend/enums/types/ElectionType.java
+++ src/main/java/edu/stonybrook/cse308/gerrybackend/enums/types/ElectionType.java
@@ -5,9 +5,9 @@ import edu.stonybrook.cse308.gerrybackend.enums.IntEnumInterface;
 import lombok.Getter;
 
 public enum ElectionType implements IntEnumInterface {
-    PRESIDENTIAL_2016(0, "ELECTION_PRES_16"),
-    CONGRESSIONAL_2016(1, "ELECTION_HOUSE_16"),
-    CONGRESSIONAL_2018(2, "ELECTION_HOUSE_18"),
+    PRESIDENTIAL_2016(0, "pres_16"),
+    CONGRESSIONAL_2016(1, "house_16"),
+    CONGRESSIONAL_2018(2, "house_18"),
     NOT_SET(3, "not_set");
 
     @Getter
